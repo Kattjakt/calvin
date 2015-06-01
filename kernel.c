ca9a66e6b9fd5315e49ec06c1f1a963ee3c1ade4
Leo Sandström <leosand97@gmail.com>
2015-06-01 20:35:05 +0200

    add kputchar() and kclrscr()

--- kernel.c
+++ kernel.c
@@ -1,21 +1,59 @@
+//#include <stdint.h>
+#include <stddef.h>
+
+#define MAX_WIDTH 80
+#define MAX_HEIGHT 25
+
+struct Screen {
+	size_t row, column;
+	char *buffer;
+};
+
+struct Screen screen;
+
+int screeninit() {
+	screen.buffer = (char*)0xB8000;
+	screen.row = 0;
+	screen.column = 0;
+}
+
+int kputchar(char c, char color) {
+	if (c == '\n' || c == '\r') {
+		screen.column = 0;
+		screen.row++;
+	} else {
+		screen.buffer[2 * (screen.row * MAX_WIDTH + screen.column)] = c;
+		screen.buffer[2 * (screen.row * MAX_WIDTH + screen.column) + 1] = color;
+		screen.column ++;
+		if (screen.column > MAX_WIDTH) {
+			screen.column = 0;
+			screen.row++;
+		}	
+	}
+	return 1; 
+}
+
+void kclrscr() {
+	screen.column = 0;
+	screen.row = 0;
+	
+	for (int i = 0; i < MAX_WIDTH * MAX_HEIGHT * 2; i++) {
+		kputchar(' ', 0x07);
+	}
+	
+	screen.column = 0;
+	screen.row = 0;
+}
+
 void kmain(void) {
-	const char *str = "CalvinOS";
-	char *vidptr = (char*)0xb8000;
-	unsigned int i = 0, j = 0;
+	screeninit();
+	kclrscr();
 
-	while (j < 80 * 25 * 2) {
-		vidptr[j] = ' ';
-		vidptr[j+1] = 0x07;
-		j = j + 2;
-	}
+	kputchar('A', 0x07);	
+	kputchar('B', 0x07);
+	kputchar('\n', 0x07);
+	kputchar('C', 0x07);
+	kputchar('D', 0x07);
 
-	j = 0;
-
-	while (str[j] != '\0') {
-		vidptr[i] = str[j];
-		vidptr[i+1] = 0x07;
-		j++;
-		i += 2;
-	}
 	return;
 }
